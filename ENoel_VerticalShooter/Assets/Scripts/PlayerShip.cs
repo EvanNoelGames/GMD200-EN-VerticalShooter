1c4cd23653d1def1350a088579bac43858f3a0f2
EvanNoelSchool <evan.noel@mymail.champlain.edu>
2024-01-22 19:58:33 -0500

    Added player health, health bar, and laser sound effect

---
+++ ENoel_VerticalShooter/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    [ContextMenu("damage")]
+    public void TakeDamage()
+    {
+        PlayerHealth.SetHealth(PlayerHealth.GetHealth() - 1);
+    }
+}
