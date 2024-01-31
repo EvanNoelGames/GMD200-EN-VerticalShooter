987b3d1e25def9e73a5fac980b4be0312ce3ac49
EvanNoelSchool <evan.noel@mymail.champlain.edu>
2024-01-31 14:59:48 -0500

    Added main menu

---
+++ ENoel_VerticalShooter/Assets/Scripts/HUD/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameObject lastSelectedButton;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+        Time.timeScale = 1f;
+        PlayerScore.SetScore(0);
+        PlayerHealth.SetHealth(3);
+        PlayerHealth.SetGameRunning(true);
+        PlayerWeaponsManager.ResetEverything();
+    }
+
+    private void Update()
+    {
+
+        if (EventSystem.current.currentSelectedGameObject != null && GetComponent<Canvas>().enabled)
+        {
+            lastSelectedButton = EventSystem.current.currentSelectedGameObject;
+        }
+
+        if (EventSystem.current.currentSelectedGameObject == null && GetComponent<Canvas>().enabled)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelectedButton);
+        }
+    }
+}
